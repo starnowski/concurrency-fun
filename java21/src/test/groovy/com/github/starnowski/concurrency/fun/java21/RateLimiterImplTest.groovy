0b397560aa34dbc931fc5c95341b893842eee7bc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-10-01 10:03:19 +0200

    Added test case for the RateLimiter

--- java21/src/test/groovy/com/github/starnowski/concurrency/fun/java21/RateLimiterImplTest.groovy
+++ java21/src/test/groovy/com/github/starnowski/concurrency/fun/java21/RateLimiterImplTest.groovy
@@ -1,6 +1,7 @@
 package com.github.starnowski.concurrency.fun.java21
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import java.time.Clock
 import java.time.Duration
@@ -10,48 +11,69 @@ class RateLimiterImplTest extends Specification {
 
     def "should accept request when no pair exists" () {
         given:
-        def clock = Mock(Clock)
-        def instant = Instant.now()
-        clock.instant() >> instant
-        def tested = new RateLimiterImpl(clock)
+            def clock = Mock(Clock)
+            def instant = Instant.now()
+            clock.instant() >> instant
+            def tested = new RateLimiterImpl(clock)
 
         when:
-        def result = tested.canAccept("x1", "0.0.0.0")
+            def result = tested.canAccept("x1", "0.0.0.0")
 
         then:
-        result
+            result
     }
 
     def "should not accept request when reaching limit" () {
         given:
-        def clock = Mock(Clock)
-        def instant = Instant.now()
-        clock.instant() >> instant
-        def tested = new RateLimiterImpl(clock, 2, Duration.ofSeconds(1000))
-        tested.canAccept("x1", "0.0.0.0")
-        tested.canAccept("x1", "0.0.0.0")
+            def clock = Mock(Clock)
+            def instant = Instant.now()
+            clock.instant() >> instant
+            def tested = new RateLimiterImpl(clock, 2, Duration.ofSeconds(1000))
+            tested.canAccept("x1", "0.0.0.0")
+            tested.canAccept("x1", "0.0.0.0")
 
         when:
-        def result = tested.canAccept("x1", "0.0.0.0")
+            def result = tested.canAccept("x1", "0.0.0.0")
 
         then:
-        !result
+            !result
+    }
+
+    @Unroll
+    def "should accept request when reaching limit for one user (#multiRequestUserAgent) but for other user limit is not reached yet (#singleRequestUserAgent)" () {
+        given:
+            def clock = Mock(Clock)
+            def instant = Instant.now()
+            clock.instant() >> instant
+            def tested = new RateLimiterImpl(clock, 2, Duration.ofSeconds(1000))
+            tested.canAccept(multiRequestUserAgent, "0.0.0.0")
+            tested.canAccept(multiRequestUserAgent, "0.0.0.0")
+
+        when:
+            def result = tested.canAccept(singleRequestUserAgent, "0.0.0.0")
+
+        then:
+            result
+
+        where:
+            multiRequestUserAgent       | singleRequestUserAgent
+            "da"                        |   "y1"
     }
 
     def "should accept request when there are no request for current slice period" () {
         given:
-        def clock = Mock(Clock)
-        def instant = Instant.now()
-        def futureInstant = instant.plus(Duration.ofSeconds(1001))
-        clock.instant() >>> [instant, instant, futureInstant ]
-        def tested = new RateLimiterImpl(clock, 2, Duration.ofSeconds(1000))
-        tested.canAccept("x1", "0.0.0.0")
-        tested.canAccept("x1", "0.0.0.0")
+            def clock = Mock(Clock)
+            def instant = Instant.now()
+            def futureInstant = instant.plus(Duration.ofSeconds(1001))
+            clock.instant() >>> [instant, instant, futureInstant ]
+            def tested = new RateLimiterImpl(clock, 2, Duration.ofSeconds(1000))
+            tested.canAccept("x1", "0.0.0.0")
+            tested.canAccept("x1", "0.0.0.0")
 
         when:
-        def result = tested.canAccept("x1", "0.0.0.0")
+            def result = tested.canAccept("x1", "0.0.0.0")
 
         then:
-        result
+            result
     }
 }
