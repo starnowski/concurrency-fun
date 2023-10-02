ffc4f71b1fe6558820e8cd7f22a7c0be7f65a75b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-10-02 21:45:47 +0200

    Added missing tests

---
+++ java21/src/test/java/com/github/starnowski/concurrency/fun/java21/RateLimiterImplJCStressTest.java
@@ -0,0 +1,36 @@
+package com.github.starnowski.concurrency.fun.java21;
+
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.I_Result;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.openjdk.jcstress.annotations.Expect.*;
+
+@JCStressTest
+@Outcome(id = "1", expect = ACCEPTABLE, desc = "One update lost.")
+@Outcome(id = "2", expect = FORBIDDEN, desc = "Both updates.")
+@State
+public class RateLimiterImplJCStressTest {
+
+    private static final String userAgent = "15";
+    private static final String ipAddress = "0.0.0.0";
+    private RateLimiter rateLimiter;
+    private Boolean[] rateResults = new Boolean[2];
+
+    @Actor
+    public void actor1() {
+        rateResults[0] = rateLimiter.canAccept(userAgent, ipAddress);
+    }
+
+    @Actor
+    public void actor2() {
+        rateResults[1] = rateLimiter.canAccept(userAgent, ipAddress);
+    }
+
+    @Arbiter
+    public void arbiter(I_Result r) {
+        r.r1 = (int) Stream.of(rateResults).filter(b -> Boolean.TRUE.equals(b)).count();
+    }
+}
