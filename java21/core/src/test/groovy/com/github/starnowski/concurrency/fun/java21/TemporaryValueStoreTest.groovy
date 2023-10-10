80c832952821d6e333762a7d3237eac03401fb5f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-10-11 00:07:55 +0200

    Added tests case [skip ci]

--- java21/core/src/test/groovy/com/github/starnowski/concurrency/fun/java21/TemporaryValueStoreTest.groovy
+++ java21/core/src/test/groovy/com/github/starnowski/concurrency/fun/java21/TemporaryValueStoreTest.groovy
@@ -25,4 +25,24 @@ class TemporaryValueStoreTest extends Specification {
         then:
             result == expectedValue
     }
+
+    def "should return temporary value when there is two invocations" () {
+        given:
+            def clock = Mock(Clock)
+            def instant = Instant.now()
+            clock.instant() >> instant
+            def expectedValue = new TemporaryValueStore.TemporaryValue("XXX", Duration.ofSeconds(300))
+            java.util.function.Supplier< TemporaryValueStore.TemporaryValue> valueSupplier = Mock(java.util.function.Supplier)
+            def tested = new TemporaryValueStore(valueSupplier, clock)
+            tested.get()
+
+        when:
+            def result = tested.get()
+
+        then:
+            result == expectedValue
+
+        and: "should invoke internal supplier only once"
+            1 * valueSupplier >> expectedValue
+    }
 }
