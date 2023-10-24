f1aa00941990835d13a9972ebad2b95dd268ebae
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-10-25 00:02:57 +0200

    Added tests case for withdraw method

--- java21/jcstress-atomicreference/src/main/java/com/github/starnowski/concurrency/fun/java21/atomicreference/CheckingAccountDoubleWithdrawJCStressTest.java
+++ java21/jcstress-atomicreference/src/main/java/com/github/starnowski/concurrency/fun/java21/atomicreference/CheckingAccountDoubleWithdrawJCStressTest.java
@@ -14,33 +14,41 @@ import static org.openjdk.jcstress.annotations.Expect.FORBIDDEN;
 @Outcome(id = "-2", expect = FORBIDDEN, desc = "Current balance was updated by first actor")
 @Outcome(id = "-3", expect = FORBIDDEN, desc = "Current balance was updated by second actor")
 @State
-@Description("CheckingAccount is executed by two actors that are trying to deposit some amount")
-public class CheckingAccountDoubleDepositJCStressTest {
+@Description("CheckingAccount is executed by two actors that are trying to withdraw some amount")
+public class CheckingAccountDoubleWithdrawJCStressTest {
 
     private final CheckingAccount tested;
 
-    public CheckingAccountDoubleDepositJCStressTest() {
-        tested = new CheckingAccount(BigDecimal.valueOf(13.00));
+    public CheckingAccountDoubleWithdrawJCStressTest() {
+        tested = new CheckingAccount(BigDecimal.valueOf(53.00));
     }
 
     @Actor
     public void actor1() {
-        tested.deposit(BigDecimal.valueOf(10));
+        try {
+            tested.withdraw(BigDecimal.valueOf(10));
+        } catch (Account.WithdrawException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Actor
     public void actor2() {
-        tested.deposit(BigDecimal.valueOf(30));
+        try {
+            tested.withdraw(BigDecimal.valueOf(30));
+        } catch (Account.WithdrawException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Arbiter
     public void arbiter(I_Result r) {
         BigDecimal currentBalance = tested.currentBalance();
-        if (currentBalance.compareTo(BigDecimal.valueOf(53)) == 0) {
+        if (currentBalance.compareTo(BigDecimal.valueOf(13)) == 0) {
             r.r1 = 1;
-        } else if (currentBalance.compareTo(BigDecimal.valueOf(23)) == 0) {
-            r.r1 = -2;
         } else if (currentBalance.compareTo(BigDecimal.valueOf(43)) == 0) {
+            r.r1 = -2;
+        } else if (currentBalance.compareTo(BigDecimal.valueOf(23)) == 0) {
             r.r1 = -3;
         } else {
             r.r1 = -1;
